85b8b60b5cb8b5574ef77b2d929b49b3d5f33781
seanredmond <sean.redmond3@student.dit.ie>
2015-04-26 21:52:59 +0000

    updated generic tab titles in helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # If don't use provide method to apply a title we can use this method to provide good default. 
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Ironmongery Store Ireland"
     if page_title.empty?
       base_title
     else
