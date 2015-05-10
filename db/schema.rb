ed019213ec7181eea49f61c3c3b872c3e643bad3
seanredmond <sean.redmond3@student.dit.ie>
2015-05-10 23:47:22 +0000

    00.47 11/05/15

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427184858) do
+ActiveRecord::Schema.define(version: 20150510232249) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "userid"
+    t.integer  "productid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
